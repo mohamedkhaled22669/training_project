505b87d4a9e33c31702abf035fc3d5e55741990e
mohamedkhaled22669 <51810133+mohamedkhaled22669@users.noreply.github.com>
2020-12-01 00:33:52 +0200

    tata

--- training/training/settings.py
+++ training/training/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'cu3c_tod1s8m5yrp9_xu@zm8nfmo0@o5ot4yz7808k!k$+9lg$'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1','mohamedkh.pythonanywhere.com']
+ALLOWED_HOSTS = ['127.0.0.1','*''mohamedkh.pythonanywhere.com']
 
 
 # Application definition
