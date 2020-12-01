87258893ca42d36f0efbb648537b5d37cecdc7f2
mohamedkhaled22669 <51810133+mohamedkhaled22669@users.noreply.github.com>
2020-12-01 14:39:37 +0200

    sada

--- training/training/settings.py
+++ training/training/settings.py
@@ -77,10 +77,14 @@ WSGI_APPLICATION = 'training.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': '<mkhost>$<mynodes>',
+        'USER': '<mkhost>',
+        'PASSWORD': '<01140305151zZ>',
+        'HOST': '<mkhost.mysql.pythonanywhere-services.com>',
     }
 }
 
