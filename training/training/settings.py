b6fc54c8d393e6d6ef79f61c9a1981bed9ed2dd0
mohamedkhaled22669 <51810133+mohamedkhaled22669@users.noreply.github.com>
2020-12-01 20:36:37 +0200

    Update settings.py

--- training/training/settings.py
+++ training/training/settings.py
@@ -151,8 +151,8 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST_USER = 'mohamedmido9913@gmail.com'
+EMAIL_HOST_USER = ''
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_PASSWORD = '01140305151'
+EMAIL_HOST_PASSWORD = ''
